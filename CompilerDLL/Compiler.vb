44eb4b773bb4a3fd86457c1b6328bf301f556577
ZeyadAhmed <zeyadahmed113@gmail.com>
2017-02-16 13:23:56 +0200

    Update Compiler.vb

--- CompilerDLL/Compiler.vb
+++ CompilerDLL/Compiler.vb
@@ -1,8 +1,8 @@
 Imports System.IO
 Namespace zscriptcompiler
-    'Z-Script Libraries and Compiler
+    'Z-Script Libraries and Compiler and Interpreter
     Public Class compiler
-
+        Dim StrVal As String
         Public Function Compile(ByVal Code As String, ByVal Address As String)
             Dim line As String
             Dim x As StreamReader
@@ -25,10 +25,37 @@ Namespace zscriptcompiler
                     Process.Start(line.Substring(14))
                     MsgBox(line.Substring(14))
                 End If
+                'Declaring a String Variable will be like ex.  SetStr <Name> { <Value>
+                If line.Contains("SetStr ") Then
+                    Dim StrVarVal As String
+                    Dim Name As String
+                    Dim sp() As String = line.Split("{")
+                    StrVarVal = sp(1)
+                    Name = sp(0).Replace("SetStr ", "")
+                    MsgBox(Name & " : " & StrVarVal)
+                    Dim StrVar As New Dictionary(Of String, String)()
+                    StrVar(Name) = StrVarVal  ' Set the value of the "variable"
+
+                    StrVal = StrVar(Name)  ' Retrieve the value of the variable
+                    MsgBox(StrVal)
+                End If
+                'Getting the value of declared string ex. GetStr <Name> @ <Print Values
+                If line.Contains("GetStr ") Then
+                    Dim GetVar As String = StrVal
+                    Dim gsp() As String = line.Split("@")
+                    If gsp(1) = "Title" Then
+                        Console.Title = GetVar
+                    End If
+                    If gsp(1) = "Mesg" Then
+                        MsgBox(GetVar)
+                    End If
+                    If gsp(1) = "Error" Then
+                        MsgBox(GetVar, MsgBoxStyle.Critical)
+                    End If
+                End If
             Loop
             Return 0
         End Function
         Public version As String = "Z-Script Compiler Version 1.0.0"
     End Class
 End Namespace
-
